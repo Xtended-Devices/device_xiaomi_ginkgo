86bc1f5c71b5d172236f8d91630aef76ac0f1450
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-12-02 15:30:56 +0530

    ginkgo: Set PRODUCT_MODEL to Redmi Note 8/8T
     * arrow website uses this flag to show device name,
       so showing only Redmi Note 8 confuses some 8T users
    
     * init_ginkgo overrides model based on device anyway

--- superior_ginkgo.mk
+++ superior_ginkgo.mk
@@ -39,6 +39,6 @@ TARGET_USES_FACE_UNLOCK := true
 PRODUCT_NAME := superior_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi Note 8
+PRODUCT_MODEL := Redmi Note 8/8T
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
