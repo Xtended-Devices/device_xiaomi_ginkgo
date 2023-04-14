a8689ec5a983baadbae9c555b6ef98d79e525e24
Charsi-Tejas <tejas@m.ms.evolution-x.org>
2023-04-14 03:07:08 +0000

    ginkgo: Bringup @Project-Xtended OFFICIAL
    Signed-off-by: Charsi-Tejas <tejas@m.ms.evolution-x.org>

---
+++ xtended_ginkgo.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) Project-Xtended
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
+# Inherit from ginkgo device
+$(call inherit-product, device/xiaomi/ginkgo/device.mk)
+
+# Inherit some common Project-Xtended stuff
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Project-Xtended Specific Flags
+XTENDED_BUILD_MAINTAINER := Tejas
+XTENDED_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_USES_BLUR := false
+
+# Device identifier
+PRODUCT_NAME := xtended_ginkgo
+PRODUCT_DEVICE := ginkgo
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 8/8T
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
