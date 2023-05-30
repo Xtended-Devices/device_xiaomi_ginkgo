a2e4e59a12a91e6102c74d07d762f5f5d7a7afff
Danny Lin <danny@kdrag0n.dev>
2023-05-30 07:54:22 +0000

    ginkgo: product: Enable Gboard side padding This adds padding to the left and right sides of the keyboard when using Gboard.
    In portrait mode, there is 4dp of padding to align the keyboard with
    some UI elements and separate the keys from the edge of the screen.
    
    In landscape mode, there is 64dp of padding to reduce the overall key
    width, which makes the keyboard significantly easier to use.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,6 +141,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
